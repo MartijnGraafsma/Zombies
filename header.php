140f508eff6575bc48a5fc883b723f8f92bcd7f5
MartijnGraafsma <91067234+MartijnGraafsma@users.noreply.github.com>
2022-01-23 17:23:00 +0100

    spul
    daws

---
+++ header.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <a href="index.php" class="logo">Zombies</a>
+            <nav>
+                <ul class="nav_links"> 
+                    <div class="dropdown">
+                    <li class="menu-item"><a href="">Black Ops 4 <i class="fas fa-caret-down"></i></a>
+                        <ul class="sub-menu">
+                            <li class="sub-item"><a href="#">Voyage Of Displair</a></li>
+                            <li class="sub-item"><a href="#">IX</a></li>
+                            <li class="sub-item"><a href="#">Blood Of The Dead</a></li>
+                            <li class="sub-item"><a href="#">Classified</a></li>
+                            <li class="sub-item"><a href="#">Dead Of The Night</a></li>
+                            <li class="sub-item"><a href="#">Ancient Evil</a></li>
+                            <li class="sub-item"><a href="#">Alpha Omega</a></li>
+                        </ul>
+                    </li>
+                    </div>
+                    <div class="dropdown">
+                    <li class="menu-item"><a href="#">Black Ops 3 <i class="fas fa-caret-down"></i></a>
+                        <ul class="sub-menu">
+                            <li class="sub-item"><a href="#">Shadows Of Evil</a></li>
+                            <li class="sub-item"><a href="#">The Giant</a></li>
+                            <li class="sub-item"><a href="#">Der Eisendrache</a></li>
+                            <li class="sub-item"><a href="#">Zetsubou No Shima</a></li>
+                            <li class="sub-item"><a href="#">Revelations</a></li>
+                        </ul>
+                    </li>
+                    </div>
+                    <div class="dropdown">
+                    <li class="menu-item"><a href="#">Black Ops 2 <i class="fas fa-caret-down"></i></a>
+                        <ul class="sub-menu">
+                            <li class="sub-item"><a href="#">Tranzit</a></li>
+                            <li class="sub-item"><a href="#">Mob Of The Dead</a></li>
+                            <li class="sub-item"><a href="Origins.html">Origins</a></li>
+                            <li class="sub-item"><a href="#">Buried</a></li>
+                            <li class="sub-item"><a href="#">Die Rise</a></li>
+                        </ul>
+                    </li>
+                    </div>
+                    <div class="dropdown">
+                    <li class="menu-item"><a href="#">Black Ops 1 <i class="fas fa-caret-down"></i></a>
+                        <ul class="sub-menu">
+                            <li class="sub-item"><a href="#">Kino Der Toten</a></li>
+                            <li class="sub-item"><a href="#">Five</a></li>
+                            <li class="sub-item"><a href="#">Ascension</a></li>
+                            <li class="sub-item"><a href="#">Call Of The Dead</a></li>
+                            <li class="sub-item"><a href="#">Shangri-La</a></li>
+                            <li class="sub-item"><a href="#">Moon</a></li>
+                            <li class="sub-item"><a href="#">Nacht Der Untoten</a></li>
+                            <li class="sub-item"><a href="#">Verrückt</a></li>
+                            <li class="sub-item"><a href="#">Shi No Numa</a></li>
+                            <li class="sub-item"><a href="#">Der Riese</a></li>
+                        </ul>
+                    </li>
+                    </div>
+                    </ul>
+               
+            </nav>
+            <label for="check" class="checkbtn"><i class="fas fa-bars" id="check"></i></label>
+        </header>
+    </div> 
+    
+    
+</body>
+</html>

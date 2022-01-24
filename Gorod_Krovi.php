dce9f652fe5bce07d94e620210ffc363448ad092
Remon Dollenkamp <remon2004@outlook.com>
2022-01-24 22:28:53 +0100

    wdas

--- Gorod_Krovi.php
+++ Gorod_Krovi.php
@@ -14,11 +14,18 @@
 
 <div class="grid">
   <div  class="item">
-    <div style='border: gray 2px solid; border-top:hidden;' class="box">
-    <center><h2 style='text-decoration:underline;'> SHIELD UPGRADE </h2></center>
-    <p>STEP 1. BUILD THE SHIELD</p>
-        <p>STEP 1. BUILD THE SHIELD</p>
- 
+    <div style='color:white;border: gray 2px solid; border-top:hidden;' class="box">
+    <center><h2 style='text-decoration:underline;'> SHIELD UPGRADE </h2> <br> <br></center>
+
+        <h4 style='text-decoration:underline;'>STEP 1.<br></h4> <p>BUILD THE SHIELD</p>
+        <p>STEP 2. GET ABOUT 40/50 KILLS WITH SHIELD, UNTILL YOU HEAR A DING</p>
+        <p>STEP 3. SHOOT THE KEEPER WRITING,<br>1st IN THE SUPPLY DEPOT <br>2nd IN THE DRAGON COMMAND AREA <br> 3rd IN THE TANK AREA</p>
+        <p>STEP 4. STAND IN THE FIRE WITH YOUR SHIELD OUT, UNTILL YOU HEAR A DING </p>
+        <p>STEP 5. CLAIM THE UPGRADED SHIELD BY THE DEAD DRAGON IN SPAWN  </p>
+
+    
+
+      
 
     </div>
   </div>

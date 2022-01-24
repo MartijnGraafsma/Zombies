4367db654bd42dfebbee691684360ddd4e581f5f
MartijnGraafsma <91067234+MartijnGraafsma@users.noreply.github.com>
2022-01-24 02:07:57 +0100

    ads

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="header.css">
 </head>
 <body>
     <div class="container">
@@ -44,7 +44,7 @@
                         <ul class="sub-menu">
                             <li class="sub-item"><a href="#">Tranzit</a></li>
                             <li class="sub-item"><a href="#">Mob Of The Dead</a></li>
-                            <li class="sub-item"><a href="#">Origins</a></li>
+                            <li class="sub-item"><a href="origins.php">Origins</a></li>
                             <li class="sub-item"><a href="#">Buried</a></li>
                             <li class="sub-item"><a href="#">Die Rise</a></li>
                         </ul>

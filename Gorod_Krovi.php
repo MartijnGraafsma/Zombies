a1ea4464750013c7968871775c22ca939838c2a8
Remon Dollenkamp <remon2004@outlook.com>
2022-01-24 23:12:24 +0100

    wdas
    wasada

--- Gorod_Krovi.php
+++ Gorod_Krovi.php
@@ -14,15 +14,19 @@
 
 <div class="grid">
   <div  class="item">
-    <div style='color:white;border: gray 2px solid; border-top:hidden;' class="box">
-    <center><h2 style='text-decoration:underline;'> SHIELD UPGRADE </h2> <br> <br></center>
+    <div style='color:white;border: gray 2px solid; border-top:hidden; border-left:hidden;' class="box">
+    <center><h2 style=' color:rgb(255, 51, 0);'> SHIELD UPGRADE </h2> <br> <br></center>
 
-        <h4 style='text-decoration:underline;'>STEP 1.<br></h4> <p>BUILD THE SHIELD</p>
+       <div class="text"> <p>STEP 1. BUILD THE SHIELD</p>
         <p>STEP 2. GET ABOUT 40/50 KILLS WITH SHIELD, UNTILL YOU HEAR A DING</p>
-        <p>STEP 3. SHOOT THE KEEPER WRITING,<br>1st IN THE SUPPLY DEPOT <br>2nd IN THE DRAGON COMMAND AREA <br> 3rd IN THE TANK AREA</p>
-        <p>STEP 4. STAND IN THE FIRE WITH YOUR SHIELD OUT, UNTILL YOU HEAR A DING </p>
-        <p>STEP 5. CLAIM THE UPGRADED SHIELD BY THE DEAD DRAGON IN SPAWN  </p>
+        <p>STEP 3. SHOOT THE KEEPER WRITING,<br>1st IN THE SUPPLY DEPOT
+         <br>2nd IN THE DRAGON COMMAND AREA
+          <br> 3rd IN THE TANK AREA<br> <img class="fire-staff-upgrade" src="Keeper_writing3.png"><img class="fire-staff-upgrade" src="Keeper_writing2.png"><img class="fire-staff-upgrade" src="Keeper_writing.png">
 
+        </p>
+        <p>STEP 4. STAND IN THE FIRE WITH YOUR SHIELD OUT, UNTILL YOU HEAR<br> A DING </p>
+        <p>STEP 5. CLAIM THE UPGRADED SHIELD BY THE DEAD DRAGON IN SPAWN  </p>
+   </div>
     
 
       
@@ -42,11 +46,11 @@
     </div>
   </div>
   <div class="item">
-    <div style='border: gray 2px solid; border-top:hidden; border-left:hidden;' class="box">
+    <div style='border: gray 2px solid; border-top:hidden; ' class="box">
     </div>
   </div>
   <div class="item">
-    <div style='border: gray 2px solid; border-left:hidden;' class="box">
+    <div style='border: gray 2px solid; ' class="box">
     </div>
   </div>
   <div class="item">

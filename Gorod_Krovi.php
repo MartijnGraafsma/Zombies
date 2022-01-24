b0a249e8d096b679272178ab2a7ff9a8f51e2693
Remon Dollenkamp <remon2004@outlook.com>
2022-01-24 03:25:17 +0100

    wdas

--- Gorod_Krovi.php
+++ Gorod_Krovi.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    
+    <link rel="stylesheet" href="GK.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gorod Krovi</title>
 </head>
@@ -12,64 +12,65 @@
         include "header.php" 
     ?>
 
- <div class="grid">
-     <div>
-    <h1>SHIELD UPGRADE</h1> <br><br>
+<div class="grid">
+  <div  class="item">
+    <div style='border: gray 2px solid; border-top:hidden;' class="box">
+    <center><h2 style='text-decoration:underline;'> SHIELD UPGRADE </h2></center>
+    <p>STEP 1. BUILD THE SHIELD</p>
+        <p>STEP 1. BUILD THE SHIELD</p>
+ 
 
-<p>
-STEP 1. BUILD THE SHIELD <br>
-
-STEP 2. GET ABOUT 40/50 KILLS WITH SHIELD, UNTILL YOU HEAR A DING<br>
-
-STEP 3. SHOOT THE KEEPER WRITING,<br>
-1st IN THE SUPPLY DEPOT<br>
-2nd IN THE DRAGON COMMAND AREA<br>
-3rd IN THE TANK AREA<br>
-
-STEP 4. STAND IN THE FIRE WITH YOUR SHIELD OUT, UNTILL YOU HEAR A DING<br>
-
-STEP 5. CLAIM THE UPGRADED SHIELD BY THE DEAD DRAGON IN SPAWN <br>
-</p>
-</div>
-
-<div>
-    <h1>SHIELD UPGRADE</h1> <br><br>
-
-<p>
-STEP 1. BUILD THE SHIELD <br>
-
-STEP 2. GET ABOUT 40/50 KILLS WITH SHIELD, UNTILL YOU HEAR A DING<br>
-
-STEP 3. SHOOT THE KEEPER WRITING,<br>
-1st IN THE SUPPLY DEPOT<br>
-2nd IN THE DRAGON COMMAND AREA<br>
-3rd IN THE TANK AREA<br>
-
-STEP 4. STAND IN THE FIRE WITH YOUR SHIELD OUT, UNTILL YOU HEAR A DING<br>
-
-STEP 5. CLAIM THE UPGRADED SHIELD BY THE DEAD DRAGON IN SPAWN <br>
-</p>
-</div>
-
-<div>
-    <h1>SHIELD UPGRADE</h1> <br><br>
-
-<p>
-STEP 1. BUILD THE SHIELD <br>
-
-STEP 2. GET ABOUT 40/50 KILLS WITH SHIELD, UNTILL YOU HEAR A DING<br>
-
-STEP 3. SHOOT THE KEEPER WRITING,<br>
-1st IN THE SUPPLY DEPOT<br>
-2nd IN THE DRAGON COMMAND AREA<br>
-3rd IN THE TANK AREA<br>
-
-STEP 4. STAND IN THE FIRE WITH YOUR SHIELD OUT, UNTILL YOU HEAR A DING<br>
-
-STEP 5. CLAIM THE UPGRADED SHIELD BY THE DEAD DRAGON IN SPAWN <br>
-</p>
-</div>
-</div>
-    
-</body>
-</html>
+    </div>
+  </div>
+  <div class="item">
+    <div style='border: gray 2px solid; border-bottom:hidden; ' class="box">
+    </div>
+  </div>
+  <div class="item">
+    <div style='border: gray 2px solid; border-top:hidden; border-right:hidden;' class="box">
+    </div>
+  </div>
+  <div class="item">
+    <div style='border: gray 2px solid; border-right:hidden;' class="box">
+    </div>
+  </div>
+  <div class="item">
+    <div style='border: gray 2px solid; border-top:hidden; border-left:hidden;' class="box">
+    </div>
+  </div>
+  <div class="item">
+    <div style='border: gray 2px solid; border-left:hidden;' class="box">
+    </div>
+  </div>
+  <div class="item">
+    <div style='border: gray 2px solid; border-left:hidden;' class="box">
+    </div>
+  </div>
+  <div class="item">
+    <div style='border: gray 2px solid; border-left:hidden; border-bottom:hidden;' class="box">
+    </div>
+  </div>
+  <div class="item">
+    <div style='border: gray 2px solid; ' class="box">
+    </div>
+    </div>
+    <div class="item">
+    <div style='border: gray 2px solid; border-bottom:hidden;' class="box">
+    </div>
+    </div>
+    <div class="item">
+    <div style='border: gray 2px solid; ' class="box">
+    </div>
+    </div>
+    <div class="item">
+    <div style='border: gray 2px solid; border-right:hidden;' class="box">
+    </div>
+    </div>
+    <div class="item">
+    <div style='border: gray 2px solid; border-bottom:hidden;' class="box">
+    </div>
+    </div>
+    <div class="item">
+    <div style='border: gray 2px solid; border-right:hidden; border-bottom:hidden;' class="box">
+    </div>
+    </div>

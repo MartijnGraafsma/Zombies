8ab85fca79de4c0362408e424d5ce6e57db40a82
Remon Dollenkamp <remon2004@outlook.com>
2022-10-29 15:02:55 +0200

    po

--- Origins/origins.php
+++ Origins/origins.php
@@ -6,74 +6,98 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Origins</title>
     <link rel="stylesheet" href="origins1.css">
+    <link rel="stylesheet" href="../CSS/base.css">
 </head>
 <body>
     <?php
         include "../Header/header.php" 
         ?>
-    <div class="fire-staff-container">
-        <h2 class="fire-staff">Fire Staff</h2>
-        <p class="fire-staff-step">STEP 1. Disc at Church</p>
-        <p class="fire-staff-step">STEP 2. Parts: Gen 6, Red Plane, First Panzer</p>
-        <p class="fire-staff-step">STEP 3. Get the Crystal at Fire tunnel near spawn</p>
-        <p class="fire-staff-step">STEP 4. Craft staff at Excavation site</p><br>
-        <h2 class="fire-staff">Upgrade</h2>
-        <p class="fire-staff-step">STEP 5. Get kills on the fire grates in Crazy place</p>
-        <p class="fire-staff-step">STEP 6. Do the puzzle at Church</p><br>
-        <img class="fire-staff-upgrade" src="fire_staff.jpg">
-        <p class="fire-staff-step">STEP 7. Line up all gems in the Excevation site and shoot the orb</p>
-        <p class="fire-staff-step">STEP 8. Place staff in the Crazy place and fill with souls</p>
+
+<div class="grid">
+    <div  class="item">
+        <div style='border: gray 2px solid; border-top:hidden; border-left:hidden;' class="box">
+            <div class="fire-staff-container">
+            <h2 class="fire-staff">Fire Staff</h2>
+            <p class="fire-staff-step">STEP 1. Disc at Church</p>
+            <p class="fire-staff-step">STEP 2. Parts: Gen 6, Red Plane, First Panzer</p>
+            <p class="fire-staff-step">STEP 3. Get the Crystal at Fire tunnel near spawn</p>
+            <p class="fire-staff-step">STEP 4. Craft staff at Excavation site</p><br>
+            <h2 class="fire-staff">Upgrade</h2>
+            <p class="fire-staff-step">STEP 5. Get kills on the fire grates in Crazy place</p>
+            <p class="fire-staff-step">STEP 6. Do the puzzle at Church</p><br>
+            <img class="fire-staff-upgrade" src="../foto's/fire_staff.jpg">
+            <p class="fire-staff-step">STEP 7. Line up all gems in the Excevation site and shoot the orb</p>
+            <p class="fire-staff-step">STEP 8. Place staff in the Crazy place and fill with souls</p>
+        </div>
+        </div>
+    </div>
+    <div class="item">
+        <div style='border: gray 2px solid; border-bottom:hidden; ' class="box">
+        <div class="ice-staff-container">
+            <h2 class="ice-staff">Ice Staff</h2>
+            <p class="ice-staff-step">STEP 1. Disc at Tank station</p>
+            <p class="ice-staff-step">STEP 2. Dig up parts when its snowing</p>
+            <p class="ice-staff-step">STEP 3. Get the Crystal at Ice tunnel near Church</p>
+            <p class="ice-staff-step">STEP 4. Craft staff at Excavation site</p><br>
+            <h2 class="ice-staff">Upgrade</h2>
+            <p class="fire-staff-step">STEP 5. Solve the puzzle in the Crazy place</p><br>
+            <img class="fire-staff-upgrade" src="../foto's/ice_staff.jpg">
+            <p class="fire-staff-step">STEP 6. Freeze 3 gravestones with the staff and shoot them with a normal gun</p>
+            <p class="fire-staff-step">- To the right of Gen 4 opposite of the soul box</p>
+            <p class="fire-staff-step">- Above footprint between Gen 4 and the Excavation site</p>
+            <p class="fire-staff-step">- Behind Gen 2 on the tank path</p>
+            <p class="fire-staff-step">STEP 7. Line up all gems in the Excevation site and shoot the orb</p>
+            <p class="fire-staff-step">STEP 8. Place staff in the Crazy place and fill with souls</p>
+        </div>
+        </div>
+    </div>
+    <div class="item">
+        <div style='border: gray 2px solid; border-top:hidden; border-right:hidden;' class="box">
+        <div class="lightning-staff-container">
+            <h2 class="lightning-staff">lightning Staff</h2>
+            <p class="lightning-staff-step">STEP 1. Disc near Gen 4</p>
+            <p class="lightning-staff-step">STEP 2. Ride the tank and collect parts</p>
+            <p class="lightning-staff-step">STEP 3. Get the Crystal at lightning tunnel near Gen 5</p>
+            <p class="lightning-staff-step">STEP 4. Craft staff at Excavation site</p><br>
+            <h2 class="lightning-staff">Upgrade</h2>
+            <p class="fire-staff-step">STEP 5. Solve The puzzle in the Crazy place</p><br>
+            <img class="fire-staff-upgrade" src="../foto's/lightning_staff.jpg">
+            <p class="fire-staff-step">STEP 6. Flip the switches around the map</p><br>
+            <img class="fire-staff-upgrade" src="../foto's/lightning_staff2.jpg">
+            <p class="fire-staff-step">STEP 7. Line up all gems in the Excevation site and shoot the orb</p>
+            <p class="fire-staff-step">STEP 8. Place staff in the Crazy place and fill with souls</p>
+        </div>
+        </div>
+    </div>
+    <div class="item">
+        <div style='border: gray 2px solid; border-right:hidden;' class="box">
+        <div class="wind-staff-container">
+            <h2 class="wind-staff">Wind Staff</h2>
+            <p class="wind-staff-step">STEP 1. Disc near Gen 5</p>
+            <p class="wind-staff-step">STEP 2. Go inside the robots for all parts</p>
+            <p class="wind-staff-step">STEP 3. Get the Crystal at Wind tunnel near Gen 4</p>
+            <p class="wind-staff-step">STEP 4. Craft staff at Excavation site</p><br>
+            <h2 class="wind-staff">Upgrade</h2>
+            <p class="fire-staff-step">STEP 5. Solve The puzzle in the Crazy place</p><br>
+            <img class="fire-staff-upgrade" src="../foto's/wind_staff.jpg">
+            <p class="fire-staff-step">STEP 6. Shoot smoke orbs around the map towards The Excavation site</p>
+            <p class="fire-staff-step">- Behind Stamin-Up machine at Gen 5</p>
+            <p class="fire-staff-step">- Between Gen 4 and Wind tunnel</p>
+            <p class="fire-staff-step">- Between Church and Gen 5 on tank path</p>
+            <p class="fire-staff-step">STEP 7. Line up all gems in the Excevation site and shoot the orb</p>
+            <p class="fire-staff-step">STEP 8. Place staff in the Crazy place and fill with souls</p><br><br>
+        </div>
+        </div>
+    </div>
+</div>
 
 
-    </div>
-    <div class="ice-staff-container">
-        <h2 class="ice-staff">Ice Staff</h2>
-        <p class="ice-staff-step">STEP 1. Disc at Tank station</p>
-        <p class="ice-staff-step">STEP 2. Dig up parts when its snowing</p>
-        <p class="ice-staff-step">STEP 3. Get the Crystal at Ice tunnel near Church</p>
-        <p class="ice-staff-step">STEP 4. Craft staff at Excavation site</p><br>
-        <h2 class="ice-staff">Upgrade</h2>
-        <p class="fire-staff-step">STEP 5. Solve the puzzle in the Crazy place</p><br>
-        <img class="fire-staff-upgrade" src="ice_staff.jpg">
-        <p class="fire-staff-step">STEP 6. Freeze 3 gravestones with the staff and shoot them with a normal gun</p>
-        <p class="fire-staff-step">- To the right of Gen 4 opposite of the soul box</p>
-        <p class="fire-staff-step">- Above footprint between Gen 4 and the Excavation site</p>
-        <p class="fire-staff-step">- Behind Gen 2 on the tank path</p>
-        <p class="fire-staff-step">STEP 7. Line up all gems in the Excevation site and shoot the orb</p>
-        <p class="fire-staff-step">STEP 8. Place staff in the Crazy place and fill with souls</p>
-    </div>
-    <div class="lightning-staff-container">
-        <h2 class="lightning-staff">lightning Staff</h2>
-        <p class="lightning-staff-step">STEP 1. Disc near Gen 4</p>
-        <p class="lightning-staff-step">STEP 2. Ride the tank and collect parts</p>
-        <p class="lightning-staff-step">STEP 3. Get the Crystal at lightning tunnel near Gen 5</p>
-        <p class="lightning-staff-step">STEP 4. Craft staff at Excavation site</p><br>
-        <h2 class="lightning-staff">Upgrade</h2>
-        <p class="fire-staff-step">STEP 5. Solve The puzzle in the Crazy place</p><br>
-        <img class="fire-staff-upgrade" src="lightning_staff.jpg">
-        <p class="fire-staff-step">STEP 6. Flip the switches around the map</p><br>
-        <img class="fire-staff-upgrade" src="lightning_staff2.jpg">
-        <p class="fire-staff-step">STEP 7. Line up all gems in the Excevation site and shoot the orb</p>
-        <p class="fire-staff-step">STEP 8. Place staff in the Crazy place and fill with souls</p>
-    </div>
-    <div class="wind-staff-container">
-        <h2 class="wind-staff">Wind Staff</h2>
-        <p class="wind-staff-step">STEP 1. Disc near Gen 5</p>
-        <p class="wind-staff-step">STEP 2. Go inside the robots for all parts</p>
-        <p class="wind-staff-step">STEP 3. Get the Crystal at Wind tunnel near Gen 4</p>
-        <p class="wind-staff-step">STEP 4. Craft staff at Excavation site</p><br>
-        <h2 class="wind-staff">Upgrade</h2>
-        <p class="fire-staff-step">STEP 5. Solve The puzzle in the Crazy place</p><br>
-        <img class="fire-staff-upgrade" src="wind_staff.jpg">
-        <p class="fire-staff-step">STEP 6. Shoot smoke orbs around the map towards The Excavation site</p>
-        <p class="fire-staff-step">- Behind Stamin-Up machine at Gen 5</p>
-        <p class="fire-staff-step">- Between Gen 4 and Wind tunnel</p>
-        <p class="fire-staff-step">- Between Church and Gen 5 on tank path</p>
-        <p class="fire-staff-step">STEP 7. Line up all gems in the Excevation site and shoot the orb</p>
-        <p class="fire-staff-step">STEP 8. Place staff in the Crazy place and fill with souls</p><br><br>
-    </div>
 
-    <div class="easter-egg-container">
+   
+  
+  
+
+    <!-- <div class="easter-egg-container">
         <h2 class="easter-egg">Easter Egg</h2>
         <p class="easter-egg-step">STEP 1. Create And upgrade all Staffs</p>
         <p class="easter-egg-step">STEP 2. Place The fire staff in the pedestall in the Excavation site (All generators must be powered)</p>
@@ -85,7 +109,7 @@
         <p class="easter-egg-step">and knife the zombies with a white glow untill a glowing stone appears</p>
         <p class="easter-egg-step">STEP 7. Place all staffs in the crazy place and kill zombies untill screen flashes white</p>
         <p class="easter-egg-step">STEP 8. Deploy the maxis Drone inside the Crazy place. A portal will appear in the center of The Crazy place. press the interaction button and your done</p>
-    </div><br><br><br><br>
+    </div><br><br><br><br> -->
 
 </body>
 </html>

006f764fa72d34166bbc9dadf37d276b98c1a52d
Remon Dollenkamp <remon2004@outlook.com>
2022-10-29 14:31:01 +0200

    paths.......

--- Gorod Krovi/Gorod_Krovi.php
+++ Gorod Krovi/Gorod_Krovi.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="GK.css">
+    <link rel="stylesheet" href="Gorod_Krovi.css">
     <link rel="stylesheet" href="../CSS/base.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gorod Krovi</title>
@@ -15,23 +15,7 @@
 
 <div class="grid">
   <div  class="item">
-    <div style='color:white;border: gray 2px solid; border-top:hidden; border-left:hidden;' class="box">
-    <center><h2 style=' color:rgb(255, 51, 0);'> SHIELD UPGRADE </h2> <br> <br></center>
-
-       <div class="text"> <p>STEP 1. BUILD THE SHIELD</p>
-        <p>STEP 2. GET ABOUT 40/50 KILLS WITH SHIELD, UNTILL YOU HEAR A DING</p>
-        <p>STEP 3. SHOOT THE KEEPER WRITING,<br>1st IN THE SUPPLY DEPOT
-         <br>2nd IN THE DRAGON COMMAND AREA
-          <br> 3rd IN THE TANK AREA<br> <img class="fire-staff-upgrade" src="Keeper_writing3.png"><img class="fire-staff-upgrade" src="Keeper_writing2.png"><img class="fire-staff-upgrade" src="Keeper_writing.png">
-
-        </p>
-        <p>STEP 4. STAND IN THE FIRE WITH YOUR SHIELD OUT, UNTILL YOU HEAR<br> A DING </p>
-        <p>STEP 5. CLAIM THE UPGRADED SHIELD BY THE DEAD DRAGON IN SPAWN  </p>
-   </div>
-    
-
-      
-
+    <div style='border: gray 2px solid; border-top:hidden; border-left:hidden;' class="box">
     </div>
   </div>
   <div class="item">
--- Header/header.php
+++ Header/header.php
@@ -7,6 +7,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="../Header/header.css">
 </head>
 <body>
     <div class="container">

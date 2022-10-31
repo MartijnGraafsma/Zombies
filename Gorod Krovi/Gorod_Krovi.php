e74d303f0d330bb2a75eeb9989be1af97683609c
MartijnGraafsma <91067234+MartijnGraafsma@users.noreply.github.com>
2022-10-31 14:51:19 +0100

    Update Gorod_Krovi.php

--- Gorod Krovi/Gorod_Krovi.php
+++ Gorod Krovi/Gorod_Krovi.php
@@ -15,26 +15,26 @@
 
 <div class="grid">
   <div  class="item">
-    <div style='border: gray 2px solid; border-top:hidden; border-left:hidden;' class="box">
+    <div class="box">
     <h1>Shield upgrade</h1><br><br>
-    <p>STEP 1. Build the shield</p><br>
-    <p>STEP 2. Get about 40/50 kills with your shield, untill you hear a ding</p><br>
+    <p>STEP 1. Build the shield</p>
+    <p>STEP 2. Get about 40/50 kills with your shield, untill you hear a ding</p>
     <p>STEP 3. Shoot the keeper writing,<br><br>
-      1st in the supply depot<br><br>     
+      1st in the supply depot<br><br>   
       <img src="../Photos\Keeper_writing.png" style="width:250px; height:150px;"><br><br>
       2nd in the dragon command area<br><br>
       <img src="../Photos\Keeper_writing2.png" style="width:250px; height:150px;"><br><br>
       3rd in the tank area<br><br>
       <img src="../Photos\Keeper_writing3.png" style="width:250px; height:150px;"><br><br>
-      <p>STEP 4. stand in the fire with your shield out, untill you hear a ding</P><br>
-      <p>STEP 5. claim the upgraded shield by the dead dragon in spawn</p>
+      <p>STEP 4. stand in the fire with your shield out, untill you hear a ding</P>
+      <p>STEP 5. claim the upgraded shield by the dead dragon in spawn</p><br>
     
   
 
     </div>
   </div>
   <div class="item">
-    <div style='border: gray 2px solid; border-bottom:hidden; ' class="box">
+    <div class="box">
     </div>
   </div>
   
